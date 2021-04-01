f77db8aa4e915c9b2b35e681b8010ddd295941ca
Radin Shayanfar <radin.shayanfar@gmail.com>
2021-04-01 17:57:33 +0430

    Resolve using additional RRs

--- project1/src/main.py
+++ project1/src/main.py
@@ -1,6 +1,7 @@
-import csv
-import socket
 import argparse
+import csv
+import re
+import socket
 
 from DNSQuery import DNSQuery
 from Question import Question
@@ -13,14 +14,32 @@ DNS_IP = "4.2.2.4"
 DNS_PORT = 53
 
 
-def resolve_dfs(query, recursion, server, print_output):
+def ns_name_to_ip(server_name, additionals):
+    ipv4_regex = r"^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
+    if re.match(ipv4_regex, server_name):
+        return server_name
+
+    if additionals is not None:
+        for additional in additionals:
+            if server_name == additional.NAME:
+                return additional.RDATA
+
+    resolved = resolve_single(server_name, Question.QTYPE_A, True, (DNS_IP, DNS_PORT), False)
+    if resolved is None:
+        raise Exception(f"{server_name} not found!")
+    return resolved.answers[0].RDATA
+
+
+def resolve_dfs(query, recursion, server, print_output, resolved_server=None):
+    if resolved_server is None:
+        resolved_server = server[0]
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.sendto(query.to_bytes(), server)
+    sock.sendto(query.to_bytes(), (resolved_server, server[1]))
     data, _ = sock.recvfrom(512)
     response = DNSQuery.from_bytes(data)
 
     if print_output:
-        print(F"Server: {server[0]}")
+        print(F"Server: {server[0]} ({resolved_server})")
         print(response)
 
     if response.header.ANCOUNT > 0:
@@ -29,9 +48,13 @@ def resolve_dfs(query, recursion, server, print_output):
         return None
 
     for auth in response.authorities:
-        resolvent = resolve_dfs(query, recursion, (auth.RDATA, DNS_PORT), print_output)
-        if resolvent is not None:
-            return resolvent
+        try:
+            resolvent = resolve_dfs(query, recursion, (auth.RDATA, DNS_PORT), print_output,
+                                    ns_name_to_ip(auth.RDATA, response.additionals))
+            if resolvent is not None:
+                return resolvent
+        except Exception as e:
+            print(str(e))
 
     return None
 
