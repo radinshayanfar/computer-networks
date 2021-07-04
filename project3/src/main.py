3a52fe8953e7c23465f71f0851d76dcf9eb06d5c
Radin Shayanfar <radin.shayanfar@gmail.com>
2021-07-05 02:27:27 +0430

    Proj3: getting network interfaces.

---
+++ project3/src/main.py
@@ -0,0 +1,35 @@
+import re
+import socket
+import subprocess
+
+CLIENT_PORT = 68
+SERVER_PORT = 67
+
+
+def get_network_interfaces() -> list:
+    output = subprocess.Popen(['ip', 'link', 'show'], stdout=subprocess.PIPE).communicate()[0].decode()
+    return re.findall(r"^\d: (.+):.*\s+link/.+ ((?:\w{2}:){5}\w{2}) ", output, re.MULTILINE)
+
+
+def select_nic(nics: list) -> tuple:
+    print("Select network interface:")
+    for i, nic in enumerate(nics):
+        print(f"[{i + 1}] {nic[0]} ({nic[1]})")
+    selected_num = int(input("-> ")) - 1
+
+    return nics[selected_num]
+
+
+if __name__ == '__main__':
+    nic_list = get_network_interfaces()
+    selected_nic = select_nic(nic_list)
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+    sock.bind(("0.0.0.0", CLIENT_PORT))
+
+    sock.sendto("test".encode(), ('<broadcast>', SERVER_PORT))
+
+    sock.close()
